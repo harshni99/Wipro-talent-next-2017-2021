94ca60f0a0c6301aefd621ef4c6e658a362c0b32
harshni99 <64963454+harshni99@users.noreply.github.com>
2020-05-31 11:07:04 +0530

    Create Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,11 @@
+public class Ascii
+{
+	public static void main(String[] args) {
+	    char ch;
+		for(int i=65;i<=122;i++)
+		{
+		    ch=(char)i;
+		    System.out.print(i+"="+ch+" ");
+		}
+	}
+}
