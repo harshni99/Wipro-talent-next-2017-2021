52df68c15f38a9eaee22b36d5d94d8ef89ff924b
harshni99 <64963454+harshni99@users.noreply.github.com>
2020-06-02 17:03:02 +0530

    Update IsOdd.java

--- IsOdd.java
+++ IsOdd.java
@@ -3,8 +3,10 @@ import java.util.*;
 public class userMainCode
 {
   public int IsOdd(int input1)
+  {
       if(input1%2!=0)
           return 2;
       else
-          return 1;
+         return 1;
+  }
 }
